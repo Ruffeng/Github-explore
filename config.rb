56af9fceff8c444a18a2e3ee52bd8649ac4627bc
Rubèn Suet <suet@mediamarktsaturn.com>
2020-03-16 18:35:28 +0100

    refactor to escale it

--- config.rb
+++ config.rb
@@ -12,6 +12,7 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/', layout: 'views/layouts/layout'
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
