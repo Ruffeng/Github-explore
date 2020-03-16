06cebc71f9f3956235f4579d58dd51c4b69f0bc2
Rubèn Suet <suet@mediamarktsaturn.com>
2020-03-16 15:53:22 +0100

    WIP - cards section

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
 activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
+  prefix.browsers = 'last 2 versions'
 end
 
 # Layouts
